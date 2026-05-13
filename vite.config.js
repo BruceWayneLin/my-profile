c01ed363b55e6e19c4776e0412d326d68c82853a
skanda <anikiwayne@gmail.com>
2026-05-13 15:34:57 +0800

    Drop console/debugger in production build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import fs from 'fs'
 import path from 'path'
 
 export default defineConfig({
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
   plugins: [
     react(),
     {
